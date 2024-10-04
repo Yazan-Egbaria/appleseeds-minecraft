fa5bc1e2a17b90eae7318a0e1f0aa8396ae8e8ac
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-10-04 18:11:45 +0300

    Finished implementing the rocks and dirts, didnt success to implement the trees though

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 const gridContainer = document.querySelector(".grid");
-const rows = 4;
+const grassRows = 1;
+const rocksRows = 2;
+const dirtRows = 2;
 const columns = 25;
 
-for (let i = 0; i < rows * columns; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  gridContainer.appendChild(cell);
+for (let i = 0; i < grassRows * columns; i++) {
+  const grassCell = document.createElement("div");
+  grassCell.classList.add("cell", "grassCell");
+  gridContainer.appendChild(grassCell);
 }
 
-gridContainer.addEventListener("click", (e) => {
-  e.target.classList.remove("cell");
-});
+for (let i = 0; i < dirtRows * columns; i++) {
+  const dirtCell = document.createElement("div");
+  dirtCell.classList.add("cell", "dirtCell");
+  gridContainer.appendChild(dirtCell);
+}
+
+for (let i = 0; i < rocksRows * columns; i++) {
+  const rockCell = document.createElement("div");
+  rockCell.classList.add("cell", "rockCell");
+  gridContainer.appendChild(rockCell);
+}
+
+// gridContainer.addEventListener("click", (e) => {
+//   e.target.classList.remove("cell");
+// });
