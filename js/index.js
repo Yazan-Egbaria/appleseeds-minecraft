f8c7e2f3c23069abaab112aeecbc505243a4fb60
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-10-05 19:25:22 +0300

    Finished both the second and the third feature of game features, 2 to go

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 // Sidebar feature
-// const sideBar = document.querySelector(".sidebar");
 const tools = document.getElementsByClassName("tool");
-const surface = document.getElementsByClassName("surface");
-
 const selectableTools = Array.from(tools);
+let selectedTool;
+
+const grassFigure = document.getElementById("grass");
+const dirtFigure = document.getElementById("dirt");
+const rockFigure = document.getElementById("rock");
+let grassCounter = document.getElementById("grassCounter");
+let dirtCounter = document.getElementById("dirtCounter");
+let rockCounter = document.getElementById("rockCounter");
+
+let grassCount = 0;
+let dirtCount = 0;
+let rockCount = 0;
+
 selectableTools.forEach((tool) => {
   tool.addEventListener("click", () => {
     if (tool.classList.contains("picked")) {
@@ -13,6 +23,7 @@ selectableTools.forEach((tool) => {
         tool.classList.remove("picked");
       });
       tool.classList.add("picked");
+      selectedTool = tool.id;
     }
   });
 });
@@ -42,6 +53,20 @@ for (let i = 0; i < rocksRows * columns; i++) {
   gridContainer.appendChild(rockCell);
 }
 
-// gridContainer.addEventListener("click", (e) => {
-//   e.target.classList.remove("cell");
-// });
+gridContainer.addEventListener("click", (e) => {
+  if (selectedTool === "shovel" && e.target.classList.contains("grassCell")) {
+    e.target.classList.remove("cell", "grassCell");
+    grassFigure.style.display = 'block'
+    return grassCounter.innerText = grassCount+=1
+  } else if (selectedTool === "shovel" && e.target.classList.contains("dirtCell")) {
+    e.target.classList.remove("cell", "dirtCell");
+    dirtFigure.style.display = 'block'
+    return dirtCounter.innerText = dirtCount+=1
+  } else if (selectedTool === "pickaxe" && e.target.classList.contains("rockCell")) {
+    e.target.classList.remove("cell", "rockCell");
+    rockFigure.style.display = 'block'
+    return rockCounter.innerText = rockCount+=1
+  } else {
+    return;
+  }
+});
